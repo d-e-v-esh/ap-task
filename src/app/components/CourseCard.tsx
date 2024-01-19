2d3a396b2ea39b07072ebe608d5e4239613e72e3
Devesh <deves125@gmail.com>
2024-01-19 16:44:38 +0530

    feat: add flag to course card

--- src/app/components/CourseCard.tsx
+++ src/app/components/CourseCard.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { devanagari } from "../layout";
+import { BiSolidTagAlt } from "react-icons/bi";
+import { cn } from "@/lib/utils";
 
 type CourseCardProps = {
   title: string;
@@ -22,14 +25,20 @@ const CourseCard = ({
   return (
     <div className="group flex cursor-pointer flex-col justify-between space-y-2 overflow-hidden px-4 pt-4 hover:bg-slate-100 laptop:rounded-lg laptop:hover:shadow">
       <div className="flex flex-col space-y-2">
-        <div className="self-start pb-2">Bhag {order}</div>
+        <div className="self-start pb-2">
+          <div className="w-12 h-6 bg-slate-400  flex justify-center items-center">
+            <span className={cn("text-white text-xs", devanagari.className)}>
+              भाग {order}
+            </span>
+          </div>
+        </div>
 
         <div className="flex w-full flex-col text-left">
           <div>
             <div className="text-lg font-medium leading-normal text-gray-title">
-              <span>{title}</span>
+              <span className={devanagari.className}>{title}</span>
             </div>
-            <div className="text-sm leading-normal text-gray-subtitle laptop:text-base">
+            <div className="text-sm leading-normal text-gray-600 laptop:text-base">
               <span>{subtitle}</span>
             </div>
             <div className="pt-1 text-xs text-gray-subtitle laptop:text-sm">
@@ -56,16 +65,16 @@ const CourseCard = ({
           <div className="inline-flex items-center space-x-3 text-xs font-medium">
             <div
               id="main-website-add-to-cart-borderless-button"
-              className="cursor-pointer rounded-md text-center transition-colors transition duration-150 btn-borderless btn-base svelte-1r7fe4c">
-              <span className="font-en svelte-t3mcl">ADD TO CART</span>
+              className="cursor-pointer rounded-md text-center transition-colors transition duration-150">
+              <span className="text-orange-600">ADD TO CART</span>
             </div>
             <div className="self-stretch py-2">
-              <div className="h-full w-px bg-gray-separator"></div>
+              <div className="h-full w-px bg-slate-900"> d</div>
             </div>
             <div
               id="main-website-enroll-in-course-button"
-              className="cursor-pointer rounded-md text-center transition-colors transition duration-150 btn-borderless btn-base svelte-1r7fe4c">
-              <span className="font-en svelte-t3mcl">ENROL</span>
+              className="cursor-pointer rounded-md text-center transition-colors transition duration-150">
+              <span className="text-orange-600">ENROL</span>
             </div>
           </div>
         </div>
