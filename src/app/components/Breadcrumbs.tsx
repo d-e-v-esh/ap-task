294b13d28d4b6f65f98452d9528a9846d29857fb
Devesh <deves125@gmail.com>
2024-01-19 16:50:28 +0530

    fix: breadcrumbs arrow

--- src/app/components/Breadcrumbs.tsx
+++ src/app/components/Breadcrumbs.tsx
@@ -1,15 +1,16 @@
 import { MdArrowForwardIos } from "react-icons/md";
 import { devanagari } from "../layout";
+
 const BreadCrumb = ({ title }: { title: string }) => {
   return (
     <div>
       <div className="hidden px-4 pt-4 tab:block laptop:px-8">
-        <div className="flex w-full flex-wrap items-baseline justify-start text-gray-subtitle">
+        <div className="flex w-full flex-wrap items-baseline justify-start text-gray-700">
           <div className="mr-1 text-sm cursor-pointer hover:text-orange-600">
             <span className="font-en svelte-t3mcl">Home</span>
           </div>
           <div className="mr-1 px-1 text-gray-subtitle">
-            <MdArrowForwardIos width={1} height={1} />
+            <MdArrowForwardIos className="w-3" />
           </div>
           <div className="mr-1 text-sm font-medium">
             <span className={devanagari.className}>{title}</span>
